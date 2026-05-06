e83e0cd5b5aff9e4ff6ff426639af1bb655b1a96
solaymanisharify <solayman.isharify@gmail.com>
2026-05-06 18:16:50 +0600

    status added

--- src/components/verifyButton/VerifyButton.jsx
+++ src/components/verifyButton/VerifyButton.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+import CustomModal from "../global/modal/CustomModal";
+
 const VerifyButton = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <div className="bg-indigo-500 text-white py-1.5 px-4 hover:bg-indigo-600 cursor-pointer rounded-full">
+      <div onClick={() => setOpen(true)} className="bg-indigo-500 text-white py-1.5 px-4 hover:bg-indigo-600 cursor-pointer rounded-full">
         <p>Verify</p>
       </div>
 
-      
+      {open && <CustomModal isCloseModal={() => setOpen(false)}></CustomModal>}
     </>
   );
 };
